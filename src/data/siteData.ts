1e330a918fb3c94a01599cad84e8e7925b2caae6
Rodri <rodrigodemonte63@gmail.com>
2026-03-13 10:00:04 -0300

    reestructura de caprtas, mejor organizacion y reparacion de secciones

---
+++ src/data/siteData.ts
@@ -0,0 +1,30 @@
+export const menu = [
+  { label: "INICIO", href: "#" },
+  { label: "QUIÉNES SOMOS", href: "#" },
+  { label: "NOTICIAS", href: "#" },
+  { label: "CONTACTO", href: "#" }
+];
+
+export const slides = [
+  {
+    image: "/images/slide1.png",
+    title: "Bienvenido a tu\nEscuela Esperanza",
+    subtitle: "Escuela Esperanza",
+    extra: "",
+    button: ""
+  },
+  {
+    image: "/images/slide2.png",
+    title: "Visita Nuestra escuela\nY encontraras esperanza",
+    subtitle: "Escuela Esperanza",
+    extra: "",
+    button: "Leer más"
+  },
+  {
+    image: "/images/slide3.png",
+    title: "Talleres\nExtraescolares 2026",
+    subtitle: "Educación básica - Escuela Esperanza",
+    extra: "Inscripciones abiertas durante ---",
+    button: "Leer más"
+  }
+];
